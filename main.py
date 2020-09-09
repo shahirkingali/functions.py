a453a2263700683ca5874475cfb15b5bf9821078
Shahir Ali <shahirsheikali@gmail.com>
2020-09-08 21:20:58 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+#Copy the above area() function definition into your functions.py file
+def area(width, height):
+  result = width * height
+  return result
+#Add two more calls to the area() function to your functions.py file.  
+print(area(4, 6))
+print(area(5, 5))
+print(area(6, 6))
+ #In your functions.py file, fill in the blanks and complete the subtract() function
+def subtract(num1, num2):
+  total = num1 - num2
+  return total
+#Add two calls to your completed subtract() function in your functions.py file and print out the results.
+print(subtract(25, 20))
+print(subtract(20, 10))
+print(subtract(70, 55))
+#Write the function definition for add() in to the functions.py file and then call add() twice with some different values and print() the results.
+def add(x, y):
+  total = x + y
+  return total
+print(add(15, 5))
